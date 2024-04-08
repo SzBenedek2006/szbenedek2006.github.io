8984409db68c0a0e392519a764350a0a34ecf84c
Benedek Szabó <bndkszabo@gmail.com>
2024-04-08 15:35:33 +0200

    Made header dissappear when button is pressed and fixed a bug where wallpaper wasn't extending to the screen

--- dynamic.js
+++ dynamic.js
@@ -38,12 +38,15 @@ img.onload = function() {
 document.addEventListener('DOMContentLoaded', function () {
             var menuButton = document.getElementById('toggle-menu');
             var menu = document.getElementById('menu');
+            var headerContents = document.querySelector('.header');
 
             menuButton.addEventListener('click', function () {
                 if (menu.style.display === 'none') {
                     menu.style.display = 'block';
+                    headerContents.style.display = 'none'; // Hide header contents
                 } else {
                     menu.style.display = 'none';
+                    headerContents.style.display = 'block'; // Show header contents
                 }
             });
         });
